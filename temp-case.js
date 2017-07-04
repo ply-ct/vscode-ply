3527e89725eccd60b98b75dc614decc2226aec74
Donald Oakes <donald@oakesville.io>
2017-07-03 20:34:43 -0600

    init

---
+++ temp-case.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// example limberest-js test case
+var limberest = require('./lib/limberest');
+var testLoc = '../limberest-demo/test';
+var caseLoc = testLoc + '/cases';
+
+var env = limberest.env(testLoc + '/limberest.io.env');
+console.log("ENV: " + limberest.stringify(env));
+
+var group = limberest.group(testLoc + '/limberest-demo.postman');
+console.log("GROUP: " + limberest.stringify(group));
+
+var test = group.test('api-docs');
+console.log("TEST: " + limberest.stringify(test));
+
+// test.run();
