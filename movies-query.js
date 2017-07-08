b97c87977b9d7f8fc87d8ca7f125ff5d05600538
Donald Oakes <donald@oakesville.io>
2017-07-08 15:27:05 -0600

    storage abstraction

--- movies-query.js
+++ movies-query.js
@@ -2,6 +2,9 @@
 
 const limberest = require('./lib/limberest');
 
+// Locations can be on the file system, at a url, or in html5 local storage.
+// The second option works seamlessly for file system or local storage.
+// const testsLoc = 'https://github.com/limberest/limberest-demo/tree/master/test';
 const testsLoc = '../limberest-demo/test';
   
 var env = limberest.env(testsLoc + '/localhost.env');
@@ -9,7 +12,7 @@ var env = limberest.env(testsLoc + '/localhost.env');
 var group = limberest.group(testsLoc + '/limberest-demo.postman');
 
 var options = {
-  caseLocation: testsLoc,
+  location: testsLoc,
   resultLocation: './results',
   logLocation: './results',
   debug: true,
