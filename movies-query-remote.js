35e763c4a174921b45af3242f2d61e29cfed6c27
Donald Oakes <donald@oakesville.io>
2017-07-15 14:38:00 -0600

    abstract storage for envs and groups

--- movies-query-remote.js
+++ movies-query-remote.js
@@ -2,8 +2,9 @@
 
 const limberest = require('./lib/limberest');
 
-//const testsLoc = '../limberest-demo/test';
 const testsLoc = 'https://raw.githubusercontent.com/limberest/limberest-demo/master/test';
+// const testsLoc = '../limberest-demo/test'; // uncomment this for async local
+
 var options = {
   location: testsLoc,
   expectedResultLocation: testsLoc + '/results/expected',
