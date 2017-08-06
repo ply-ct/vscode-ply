c2018fc56ae97650a13b033f18830af775c9a190
Donald Oakes <donald@oakesville.io>
2017-08-06 15:44:16 -0600

    better logging

--- movies-query-local.js
+++ movies-query-local.js
@@ -4,14 +4,6 @@ const limberest = require('./lib/limberest');
 
 // Note testsLoc on file system allows synchronous reads.
 const testsLoc = '../limberest-demo/test';
-var options = {
-  location: testsLoc,
-  expectedResultLocation: testsLoc + '/results/expected',
-  resultLocation: testsLoc + '/results/actual',
-  debug: true,
-  responseHeaders: ['content-type']
-};
-  
 var values = limberest.loadValuesSync(testsLoc + '/limberest.io.env');
 var group = limberest.loadGroupSync(testsLoc + '/movies-api.postman');
 
@@ -20,6 +12,14 @@ var test = group.getTest('GET', 'movies?{query}');
 values = Object.assign({}, values);
 values.query = 'year=1935&rating=5';
 
+var options = {
+  location: testsLoc,
+  expectedResultLocation: testsLoc + '/results/expected',
+  resultLocation: testsLoc + '/results/actual',
+  debug: true,
+  responseHeaders: ['content-type']
+};
+    
 test.run(options, values, (error, response) => {
   test.verify(values, (err, result) => {
     if (err)
