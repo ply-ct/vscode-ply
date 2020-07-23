5f90955a7bc96a1898c75cef9b8ec1389b1aa00e
Donald Oakes <donald.h.oakes@gmail.com>
2020-07-23 10:43:20 -0600

    fix test

--- test/plyTests.test.ts
+++ test/plyTests.test.ts
@@ -1,19 +1,20 @@
 import * as assert from 'assert';
+import { URI as Uri } from 'vscode-uri';
 import { PlyRoot } from '../src/plyRoots';
 import * as help from './help';
 
 describe('vs-code ply tests', function () {
 
     it('should be grouped', async function() {
-        let plyRoot = new PlyRoot(help.workspaceFolderUri, 'requests', 'Requests');
-        let plyableUris = [
-            plyRoot.toUri('src/test/ply/up-here.request.yaml#oneUp'),
-            plyRoot.toUri('src/test/ply/requests/create-movie.request.yaml#createMovie'),
-            plyRoot.toUri('src/test/ply/requests/delete-movie.request.yaml#deleteMovie'),
-            plyRoot.toUri('src/test/plyables/x.request.yaml#reqX1'),
-            plyRoot.toUri('src/test/plyables/x.request.yaml#reqX2'),
-            plyRoot.toUri('src/test/plyables/y.request.yaml#reqY'),
-            plyRoot.toUri('test/ply/requests/a.request.yaml#reqA'),
+        const plyRoot = new PlyRoot(help.workspaceFolderUri, 'requests', 'Requests');
+        const plyableUris: [Uri, number][] = [
+            [plyRoot.toUri('src/test/ply/up-here.request.yaml#oneUp'), 0],
+            [plyRoot.toUri('src/test/ply/requests/create-movie.request.yaml#createMovie'), 0],
+            [plyRoot.toUri('src/test/ply/requests/delete-movie.request.yaml#deleteMovie'), 0],
+            [plyRoot.toUri('src/test/plyables/x.request.yaml#reqX1'), 0],
+            [plyRoot.toUri('src/test/plyables/x.request.yaml#reqX2'), 0],
+            [plyRoot.toUri('src/test/plyables/y.request.yaml#reqY'), 0],
+            [plyRoot.toUri('test/ply/requests/a.request.yaml#reqA'), 0]
         ];
         plyRoot.build(plyableUris);
 
@@ -37,16 +38,6 @@ describe('vs-code ply tests', function () {
         a.request.yaml
             - reqA
 `);
-    });
-
-    it('should be cool', async function() {
-        console.log("I'm cool");
-        assert.equal("ain't cool", 'cool');
-    });
-
-    it('should fail', async function() {
-        console.log('Be cool');
-        assert.equal('not cool', 'cool');
     });
 
 });
