7588ab675522ac12483c75f282bba158eaef3288
Donald Oakes <donald.h.oakes@gmail.com>
2021-01-01 19:31:24 -0700

    cleanup

--- src/flow/editor.ts
+++ src/flow/editor.ts
@@ -161,9 +161,6 @@ export class FlowEditor implements vscode.CustomTextEditorProvider {
                 this.compareResults(document.uri, message.target);
             } else if (message.type === 'instance') {
                 updateWebview(this.getInstance(document.uri));
-            } else if (message.type === 'paste') {
-                const txt = await vscode.env.clipboard.readText();
-                console.log("PASTING: " + txt);
             }
         }));
 
