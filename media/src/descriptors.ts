33ac3aa9053c4ec8d4a84453ca39034ed7ba3765
Donald Oakes <donald.h.oakes@gmail.com>
2022-02-26 11:07:09 -0700

    merge script-step

--- media/src/descriptors.ts
+++ media/src/descriptors.ts
@@ -11,6 +11,12 @@ export const descriptors: flowbee.Descriptor[] = [
         path: 'request',
         name: 'Request',
         icon: 'request.svg'
+    },
+    {
+        type: 'step',
+        path: 'typescript',
+        name: 'TypeScript',
+        icon: 'typescript.svg'
     }
     /*
     {
