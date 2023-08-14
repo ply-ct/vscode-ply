beaf7f3a391032688172e57549ffb2a9814f45c8
Donald Oakes <donald.h.oakes@gmail.com>
2023-08-13 19:26:59 -0600

    test

--- test/ply-roots.test.ts
+++ test/ply-roots.test.ts
@@ -17,14 +17,15 @@ describe('ply roots', function () {
 
     const flowRoot = new PlyRoot(help.workspaceFolderUri, 'flows', 'Flows');
     const flowUris: [Uri, number][] = [
-        [flowRoot.toUri('src/test/ply/flow-here.ply.flow'), 0],
-        [flowRoot.toUri('src/test/ply/flows/create-movies.ply.flow#s2'), 0],
-        [flowRoot.toUri('src/test/ply/flows/delete-movie.ply.flow'), 0],
-        [flowRoot.toUri('src/test/plyables/a.ply.flow'), 0],
+        [flowRoot.toUri('src/test/ply/flow-here.ply.flow#s1'), 0],
+        [flowRoot.toUri('src/test/ply/flow-here.ply.flow#s2'), 0],
+        [flowRoot.toUri('src/test/ply/flows/create-movies.ply.flow#s1'), 0],
+        [flowRoot.toUri('src/test/ply/flows/delete-movie.ply.flow#s2'), 0],
+        [flowRoot.toUri('src/test/plyables/z.ply.flow#s1'), 0],
+        [flowRoot.toUri('src/test/plyables/z.ply.flow#s2'), 0],
         [flowRoot.toUri('src/test/plyables/a.ply.flow#s3'), 0],
-        [flowRoot.toUri('src/test/plyables/y.ply.flow'), 0],
-        [flowRoot.toUri('src/test/plyables/y.ply.flow#s4'), 0],
-        [flowRoot.toUri('test/ply/requests/in-requests.ply.flow'), 0]
+        [flowRoot.toUri('src/test/plyables/a.ply.flow#s1'), 0],
+        [flowRoot.toUri('test/ply/requests/in-requests.ply.flow#s1'), 0]
     ];
 
     it('should be grouped by location', () => {
@@ -55,24 +56,51 @@ describe('ply roots', function () {
 
     it('should merge children across roots', () => {
         const plyRoots = new PlyRoots(help.workspaceFolderUri);
-        // requestRoot.build(requestUris);
-
-        flowUris.sort((u1, u2) => {
-            const segs1 = u1[0].path.split('/');
-            const segs2 = u2[0].path.split('/');
-            return segs1.length - segs2.length;
-        });
-
+        requestRoot.build(requestUris);
         flowRoot.build(flowUris);
 
-        console.log('flowRoot: ' + flowRoot.toString());
+        plyRoots.rootSuite.children = [];
+        plyRoots.merge(plyRoots.rootSuite, requestRoot.baseSuite.children);
+        plyRoots.merge(plyRoots.rootSuite, flowRoot.baseSuite.children);
 
-        // plyRoots.rootSuite.children = [];
-        // plyRoots.merge(plyRoots.rootSuite, requestRoot.baseSuite.children);
-        // plyRoots.merge(plyRoots.rootSuite, flowRoot.baseSuite.children);
+        plyRoots.sort(plyRoots.rootSuite);
 
-        // plyRoots.sort(plyRoots.rootSuite);
-
-        // console.log('MERGED: ' + plyRoots.toString());
+        assert.strictEqual(
+            plyRoots.toString(),
+            `    src/test/ply
+        flow-here.ply.flow
+            - s1
+            - s2
+        up-here.request.yaml
+            - oneUp
+    src/test/ply/flows
+        create-movies.ply.flow
+            - s1
+        delete-movie.ply.flow
+            - s2
+    src/test/ply/requests
+        create-movie.request.yaml
+            - createMovie
+        delete-movie.request.yaml
+            - deleteMovie
+    src/test/plyables
+        a.ply.flow
+            - s3
+            - s1
+        x.request.yaml
+            - reqX1
+            - reqX2
+        y.request.yaml
+            - reqY
+        z.ply.flow
+            - s1
+            - s2
+    test/ply/requests
+        a.request.yaml
+            - reqA
+        in-requests.ply.flow
+            - s1
+`
+        );
     });
 });
